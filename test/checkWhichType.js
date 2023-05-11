469cac498a7e9616a2a14825579b0489db5e1f72
alan yen <alan830719@gmail.com>
2023-05-11 14:22:53 +0800

    a bit change

--- test/checkWhichType.js
+++ test/checkWhichType.js
@@ -5,25 +5,25 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
+const ERC721interface = 0x80ac58cd;
+const ERC1155interface = 0xd9b67a26;
+const AccessControlinterface = 0x7965db0b;
+const fallbackInterface = 0xffffffff;
+
 describe("check which type", function () {
   async function deployedContract() {
 
-    const ERC721interface = 0x80ac58cd;
-    const ERC1155interface = 0xd9b67a26;
-    const AccessControlinterface = 0x7965db0b;
-    const fallbackInterface = 0xffffffff;
-
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const _theContract = await ethers.getContractFactory("Zapper_NFT_V2_0_1"); //合約名稱
-    const theContract = await _theContract.deploy("s","s","s",owner.address,owner.address,10); //部署合約參數
+    const _theContract = await ethers.getContractFactory("ERC1"); //合約名稱
+    const theContract = await _theContract.deploy("j",5,5,5,5,5); //部署合約參數
 
-    return { theContract, ERC721interface, ERC1155interface, AccessControlinterface, fallbackInterface, owner};
+    return { theContract, owner};
   }
 
   describe("test", function () {
     it("Is ERC721 ?", async function () {
-      const { theContract,ERC721interface,fallbackInterface } = await loadFixture(deployedContract);
+      const { theContract} = await loadFixture(deployedContract);
       let result =false;
       if (await theContract.supportsInterface(ERC721interface)){
         if (!(await theContract.supportsInterface(fallbackInterface))){
@@ -34,7 +34,7 @@ describe("check which type", function () {
     });
 
     it("Is ERC1155 ?", async function () {
-      const { theContract, ERC1155interface,fallbackInterface } = await loadFixture(deployedContract);
+      const { theContract} = await loadFixture(deployedContract);
       let result =false;
       if (await theContract.supportsInterface(ERC1155interface)){
         if (!(await theContract.supportsInterface(fallbackInterface))){
@@ -52,7 +52,7 @@ describe("check which type", function () {
       let hasDecimals = false;
       if(typeof decimals == typeof 10){ //一般decimals回傳值是uint8
         hasDecimals = true;
-      }else if(typeof decimals== typeof ethers.utils.parseEther("1")){ //有些合約decimals回傳值卻是uint256
+      }else if(typeof decimals== typeof ethers.utils.parseEther("1")){ //有些合約decimals回傳值卻是uint256  像是USDT
         hasDecimals = true;
       }
       expect(typeof name).to.equal(typeof "String");
@@ -61,7 +61,7 @@ describe("check which type", function () {
     });
 
     it("Is AccessControl ?", async function () {
-      const { theContract, AccessControlinterface,fallbackInterface } = await loadFixture(deployedContract);
+      const { theContract} = await loadFixture(deployedContract);
       let result =false;
       if (await theContract.supportsInterface(AccessControlinterface)){
         if (!(await theContract.supportsInterface(fallbackInterface))){
@@ -72,7 +72,7 @@ describe("check which type", function () {
     });
 
     it("Is Ownable ?", async function () {
-      const { theContract,owner} = await loadFixture(deployedContract);
+      const { theContract, owner} = await loadFixture(deployedContract);
       const ownerAdd= await theContract.owner();
       expect(ownerAdd).to.equal(owner.address);
     });
